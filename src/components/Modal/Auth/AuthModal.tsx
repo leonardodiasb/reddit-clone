2ec848a022ee4d8ac0bb9560d727924696b161a7
leonardo dias <leonardodias@ucl.br>
2023-02-03 17:50:53 -0300

    create oauth button

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,8 +1,9 @@
 import { authModalState } from "@/atoms/authModalAtom";
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Flex } from "@chakra-ui/react";
+import { Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Flex } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
 import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButtons";
 
 const AuthModal:React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
@@ -41,7 +42,8 @@ const AuthModal:React.FC = () => {
               justify='center'
               width='70%'
             >
-              {/* <OAuthButtons /> */}
+              <OAuthButtons />
+              <Text color='gray.500' fontWeight={700}>OR</Text>
               <AuthInputs />
               {/* <ResetPassword /> */}
             </Flex>
