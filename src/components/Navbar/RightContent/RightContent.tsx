11ce96bd4143933a4d6edcb18e472609fbc24aec
leonardo dias <leonardodias@ucl.br>
2023-02-04 18:28:44 -0300

    create navbar icons

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,13 @@
 import AuthModal from "@/components/Modal/Auth/AuthModal";
 import { auth } from "@/firebase/clientApp";
 import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { signOut, User } from "firebase/auth";
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
 
 type Props = {
-  user: any;
+  user?: User | null;
 };
 
 const RightContent: React.FC<Props> = ({ user }) => {
@@ -14,7 +15,11 @@ const RightContent: React.FC<Props> = ({ user }) => {
     <>
       <AuthModal />
       <Flex justify='center' align='center'>
-        {user ? <Button onClick={() => signOut(auth)}>Logout</Button> : <AuthButtons />}
+        {user ? (
+          <Icons />
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
