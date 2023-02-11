b609a8bf49bbaa1d39e8a5625c8becd3ad62939b
leonardo dias <leonardodias@ucl.br>
2023-02-11 15:44:05 -0300

    add change community image functionality

--- src/components/Community/About.tsx
+++ src/components/Community/About.tsx
@@ -1,18 +1,53 @@
-import { Community } from "@/atoms/communitiesAtom";
-import { Box, Flex, Text, Icon, Stack, Divider, Button } from "@chakra-ui/react";
-import React from "react";
+import { Community, communityState } from "@/atoms/communitiesAtom";
+import { Box, Flex, Text, Icon, Stack, Divider, Button, Image, Spinner } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
 import { RiCakeLine } from "react-icons/ri";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import moment from "moment";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore, storage } from "@/firebase/clientApp";
+import useSelectFile from "@/hooks/useSelectFile";
+import { FaReddit } from "react-icons/fa";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { doc, updateDoc } from "firebase/firestore";
+import { useSetRecoilState } from "recoil";
 
 type AboutProps = {
   communityData: Community
 };
 
 const About:React.FC<AboutProps> = ({ communityData }) => {
-  const router = useRouter();
+  const [user] = useAuthState(auth);
+  const selectedFileRef = useRef<HTMLInputElement>(null);
+  const { selectedFile, setSelectedFile, onSelectFile } = useSelectFile();
+  const [uploadingImage, setUploadingImage] = useState(false);
+  const setCommunityStateValue = useSetRecoilState(communityState);
+
+  const onUpdateImage = async () => {
+    if (!selectedFile) return;
+    setUploadingImage(true);
+    try {
+      const imageRef = ref(storage, `communities/${communityData.id}/image`);
+      await uploadString(imageRef, selectedFile, "data_url");
+      const downloadURL = await getDownloadURL(imageRef);
+      await updateDoc(doc(firestore, "communities", communityData.id), {
+        imageURL: downloadURL,
+      });
+
+      setCommunityStateValue(prev => ({
+        ...prev,
+        currentCommunity: {
+          ...prev.currentCommunity,
+          imageURL: downloadURL
+        } as Community
+      }));
+
+    } catch (error: any) {
+      console.log("onUpdateImage error", error.message);
+    }
+    setUploadingImage(false);
+  };
 
   return (
     <Box position='sticky' top='14px'>
@@ -51,9 +86,56 @@ const About:React.FC<AboutProps> = ({ communityData }) => {
               </Text>
             )}
           </Flex>
-          <Link href={`/r/${router.query.communityId}/submit`}>
+          <Link href={`/r/${communityData.id}/submit`}>
             <Button mt={3} height='30px' width='100%'>Create Post</Button>
           </Link>
+          {communityData.creatorId === user?.uid && (
+            <>
+              <Divider />
+              <Stack spacing={1} fontSize='10pt'>
+                <Text fontWeight={600}>Admin</Text>
+                <Flex align='center' justify='space-between'>
+                  <Text
+                    color='blue.500'
+                    cursor='pointer'
+                    _hover={{ textDecoration: "underline" }}
+                    onClick={() => selectedFileRef.current?.click()}
+                  >
+                    Change Image
+                  </Text>
+                  {communityData.imageURL || selectedFile ? (
+                    <Image
+                      src={selectedFile || communityData.imageURL}
+                      borderRadius='full'
+                      boxSize='40px'
+                      alt='Community Image'
+                    />
+                  ) : (
+                    <Icon
+                      as={FaReddit}
+                      fontSize={40}
+                      color='brand.100'
+                      mr={2}
+                    />
+                  )}
+                </Flex>
+                {selectedFile && 
+                  (uploadingImage ? (
+                    <Spinner /> 
+                  ) : (
+                    <Text cursor='pointer' onClick={onUpdateImage}>Save Changes</Text>
+                  ))}
+                <input 
+                  id="file-upload"
+                  type='file'
+                  accept="image/x-png,image/gif,image/jpeg"
+                  hidden
+                  ref={selectedFileRef} 
+                  onChange={onSelectFile}
+                />
+              </Stack>
+            </>
+          )}
         </Stack>
 
       </Flex>
--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/router";
 import { addDoc, collection, serverTimestamp, Timestamp, updateDoc } from "firebase/firestore";
 import { firestore, storage } from "@/firebase/clientApp";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import useSelectFile from "@/hooks/useSelectFile";
 
 type NewPostFormProps = {
   user: User 
@@ -52,7 +53,7 @@ const NewPostForm:React.FC<NewPostFormProps> = ({ user }) => {
     title: "",
     body: ""
   });
-  const [selectedFile, setSelectedFile] = useState<string>();
+  const { selectedFile, setSelectedFile, onSelectFile } = useSelectFile();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -62,7 +63,7 @@ const NewPostForm:React.FC<NewPostFormProps> = ({ user }) => {
 
     const newPost: PostType = {
       communityId: communityId as string,
-      creatorId: user?.uid,
+      creatorId: user.uid,
       creatorDisplayName: user.email!.split("@")[0],
       title: textInputs.title,
       body: textInputs.body,
@@ -96,20 +97,6 @@ const NewPostForm:React.FC<NewPostFormProps> = ({ user }) => {
     setLoading(false);
   };
    
-  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-
-    if (event.target.files?.[0]) {
-      reader.readAsDataURL(event.target.files[0]);
-    }
-
-    reader.onload = (readerEvent) => {
-      if (readerEvent.target?.result) {
-        setSelectedFile(readerEvent.target.result as string);
-      }
-    };
-  };
-
   const onTextChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { target: { name, value } } = event;
     setTextInputs(prev => ({
@@ -142,7 +129,7 @@ const NewPostForm:React.FC<NewPostFormProps> = ({ user }) => {
         {selectedTab === "Images & Video" && (
           <ImageUpload 
             selectedFile={selectedFile} 
-            onSelectImage={onSelectImage} 
+            onSelectImage={onSelectFile} 
             setSelectedTab={setSelectedTab} 
             setSelectedFile={setSelectedFile} 
           />
