37109879b1c6de34c2bc3160c378d38662fd1f7b
leonardo dias <leonardodias@ucl.br>
2023-02-14 18:00:12 -0300

    create top communities and extra components for home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { PostType, PostVote } from "@/atoms/postsAtom";
 import CreatePostLink from "@/components/Community/CreatePostLink";
+import PersonalHome from "@/components/Community/PersonalHome";
+import Premium from "@/components/Community/Premium";
+import Recommendations from "@/components/Community/Recommendations";
 import PageContent from "@/components/Layout/PageContent";
 import PostItem from "@/components/Posts/PostItem";
 import PostLoader from "@/components/Posts/PostLoader";
@@ -132,9 +135,11 @@ export default function Home() {
           </Stack>
         )}
       </>
-      <>
-        {/* recommnedations */}
-      </>
+      <Stack spacing={5}>
+        <Recommendations />
+        <Premium />
+        <PersonalHome />
+      </Stack>
     </PageContent>
   );
 }
